23bf65d0b55e5e539f0e57f95733cf9853469f80
mgmchan <mgmchan@bcil.com>
2020-05-07 23:31:12 +0800

    added command for Ingress-NGINX in Docker Desktop K8s

--- k8s-secret.bat
+++ k8s-secret.bat
@@ -1,2 +1,5 @@
 kubectl create secret generic pgpassword --from-literal MYPASSWORD=Ab123456
 secret/pgpassword created
+
+# Following to set up Ingress NGINX on Docker Kubernetes
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/cloud/deploy.yaml
